b55ce3df18912b47fd8b7d8835c859cb6866db11
KeanoW <winterkeano02@gmail.com>
2022-07-18 20:36:58 +0200

    Got website layout done

--- server.py
+++ server.py
@@ -1,7 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+# @app.route("/")
+# def home():
+#     return render_template("templates/index.html")
+
+@app.route("/cipher")
+def contact():
+    return render_template("/cipher.html")
+
+@app.route("/about")
+def about():
+    return render_template("/about.html")
+
+@app.route("/components")
+def components():
+    return render_template("/components.html")
